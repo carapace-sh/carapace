006622663e27c9bbddc3b4b477f2e22d6d620cb4
rsteube <rsteube@users.noreply.github.com>
2020-09-12 20:01:48 +0200

    added oil shell (alias for bash)
    not yet working though

--- carapace.go
+++ carapace.go
@@ -97,12 +97,14 @@ func (c Carapace) Snippet(shell string) string {
 		snippet = elvish.Snippet
 	case "fish":
 		snippet = fish.Snippet
+	case "osh":
+		snippet = bash.Snippet
 	case "powershell":
 		snippet = powershell.Snippet
 	case "zsh":
 		snippet = zsh.Snippet
 	default:
-		return fmt.Sprintf("expected 'bash', 'elvish', 'fish', 'powershell' or 'zsh' [was: %v]", shell)
+		return fmt.Sprintf("expected 'bash', 'elvish', 'fish', 'osh', 'powershell' or 'zsh' [was: %v]", shell)
 	}
 	return snippet(c.cmd.Root(), completions.actions.Shell(shell))
 }
@@ -219,14 +221,16 @@ func determineShell() string {
 				return "elvish"
 			case "fish":
 				return "fish"
+			case "osh":
+				return "osh"
 			case "pwsh":
 				return "powershell"
 			case "zsh":
 				return "zsh"
 			default:
+				return ""
 			}
 		}
-
 	}
 }
 

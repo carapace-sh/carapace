f6c8a88c96db2dc28066a7d12813852e10488cca
rsteube <rsteube@users.noreply.github.com>
2020-12-25 02:06:23 +0100

    removed hightlighting

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -4,24 +4,11 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
 	"testing"
 )
 
-func Highlight(s string) string {
-	cmd := exec.Command("bat", "-l", "bash", "--color", "always", "--wrap", "never")
-	cmd.Stdin = strings.NewReader(s)
-	if output, err := cmd.Output(); err != nil {
-		return s // no bat command
-	} else {
-		return string(output)
-	}
-}
-
 func Equal(t *testing.T, expected string, actual string) {
-	if expected == actual {
-		t.Log("\n" + Highlight(actual))
-	} else {
+	if expected != actual {
 		expectedFile, _ := ioutil.TempFile(os.TempDir(), "carapace_test")
 		actualFile, _ := ioutil.TempFile(os.TempDir(), "carapace_test")
 

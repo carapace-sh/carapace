d8140abad9ce614285e732155fa9470c4322f1b0
rsteube <rsteube@users.noreply.github.com>
2021-03-10 11:18:52 +0100

    fish: re-enabled special characters

--- internal/fish/action.go
+++ internal/fish/action.go
@@ -1,33 +1,21 @@
 package fish
 
 import (
+	"fmt"
 	"github.com/rsteube/carapace/internal/common"
 	"strings"
 )
 
 var sanitizer = strings.NewReplacer(
-	`$`, ``,
-	"`", ``,
 	"\n", ``,
-	`\`, ``,
-	`"`, ``,
-	`'`, ``,
-	`(`, `[`,
-	`)`, `]`,
+	"\r", ``,
+	"\t", ``,
 )
-
-func Sanitize(values ...string) []string {
-	sanitized := make([]string, len(values))
-	for index, value := range values {
-		sanitized[index] = sanitizer.Replace(value)
-	}
-	return sanitized
-}
 
 func ActionRawValues(callbackValues string, values ...common.RawValue) string {
 	vals := make([]string, len(values))
 	for index, val := range values {
-		vals[index] = sanitizer.Replace(val.Value) + "\t" + sanitizer.Replace(val.Description)
+		vals[index] = fmt.Sprintf("%v\t%v", sanitizer.Replace(val.Value), sanitizer.Replace(val.Description))
 	}
 	return strings.Join(vals, "\n")
 }

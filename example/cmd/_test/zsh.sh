314577bd49cb79528053caae3e0a13f3e58a1e9f
rsteube <rsteube@users.noreply.github.com>
2021-03-25 11:54:51 +0100

    zsh re-enabled special characters

--- example/cmd/_test/zsh.sh
+++ example/cmd/_test/zsh.sh
@@ -1,13 +1,24 @@
 #compdef example
 function _example_completion {
   local IFS=$'\n'
-  # shellcheck disable=SC2207,SC2086
-  local c=($(example _carapace zsh _ ${^words//\\ / }''))
+  
+  # shellcheck disable=SC2207,SC2086,SC2154
+  if echo ${words}"''" | xargs echo 2>/dev/null > /dev/null; then
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words}"''" | xargs example _carapace zsh _ ))
+  elif echo ${words} | sed "s/\$/'/" | xargs echo 2>/dev/null > /dev/null; then
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words} | sed "s/\$/'/" | xargs example _carapace zsh _ ))
+  else
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words} | sed 's/$/"/'  | xargs example _carapace zsh _ ))
+  fi
+
   # shellcheck disable=SC2034,2206
   local vals=(${c%%$'\t'*})
   # shellcheck disable=SC2034,2206
   local descriptions=(${c##*$'\t'})
-  compadd -Q -S '' -d descriptions -a -- vals
+  compadd -S '' -d descriptions -a -- vals
 }
 compquote '' 2>/dev/null && _example_completion
 compdef _example_completion example
--- internal/zsh/snippet.go
+++ internal/zsh/snippet.go
@@ -2,34 +2,35 @@ package zsh
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/rsteube/carapace/internal/uid"
 	"github.com/spf13/cobra"
-)
-
-var replacer = strings.NewReplacer(
-	"\n", ``,
-	"`", `\"`,
-	`:`, `\:`,
-	`"`, `\"`,
-	`[`, `\[`,
-	`]`, `\]`,
 )
 
 func Snippet(cmd *cobra.Command) string {
 	return fmt.Sprintf(`#compdef %v
 function _%v_completion {
   local IFS=$'\n'
-  # shellcheck disable=SC2207,SC2086
-  local c=($(%v _carapace zsh _ ${^words//\\ / }''))
+  
+  # shellcheck disable=SC2207,SC2086,SC2154
+  if echo ${words}"''" | xargs echo 2>/dev/null > /dev/null; then
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words}"''" | xargs %v _carapace zsh _ ))
+  elif echo ${words} | sed "s/\$/'/" | xargs echo 2>/dev/null > /dev/null; then
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words} | sed "s/\$/'/" | xargs %v _carapace zsh _ ))
+  else
+    # shellcheck disable=SC2207,SC2086
+    local c=($(echo ${words} | sed 's/$/"/'  | xargs %v _carapace zsh _ ))
+  fi
+
   # shellcheck disable=SC2034,2206
   local vals=(${c%%%%$'\t'*})
   # shellcheck disable=SC2034,2206
   local descriptions=(${c##*$'\t'})
-  compadd -Q -S '' -d descriptions -a -- vals
+  compadd -S '' -d descriptions -a -- vals
 }
 compquote '' 2>/dev/null && _%v_completion
 compdef _%v_completion %v
-`, cmd.Name(), cmd.Name(), uid.Executable(), cmd.Name(), cmd.Name(), cmd.Name())
+`, cmd.Name(), cmd.Name(), uid.Executable(), uid.Executable(), uid.Executable(), cmd.Name(), cmd.Name(), cmd.Name())
 }

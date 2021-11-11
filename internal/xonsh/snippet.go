1bcecb40e9e8ce95b8e709a99f9929ec52ad96f8
rsteube <rsteube@users.noreply.github.com>
2021-11-11 14:22:48 +0100

    Revert "xonsh: prevent file completion fallback"
    This reverts commit eae2919d28cb68f450267e59414f3a6fc307d63b.

--- internal/xonsh/snippet.go
+++ internal/xonsh/snippet.go
@@ -22,12 +22,9 @@ def _%v_completer(context):
         from xonsh.completers.tools import RichCompletion
         output, _ = Popen(['%v', '_carapace', 'xonsh', '_', *[a.value for a in context.args], context.prefix], stdout=PIPE, stderr=PIPE).communicate()
         try:
-            result = {RichCompletion(c["Value"], display=c["Display"], description=c["Description"], prefix_len=len(context.raw_prefix)) for c in loads(output)}
+            return {RichCompletion(c["Value"], display=c["Display"], description=c["Description"], prefix_len=len(context.raw_prefix)) for c in loads(output)}
         except:
-            result = {}
-        if len(result) == 0:
-            result = {RichCompletion(context.prefix, display=context.prefix, description='', prefix_len=len(context.raw_prefix))}
-        return result
+            return
 
 
 from xonsh.completers._aliases import _add_one_completer

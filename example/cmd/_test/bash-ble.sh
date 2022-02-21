f4dfe52011076c07103115dea0218025760c1150
rsteube <rsteube@users.noreply.github.com>
2022-02-21 12:18:48 +0100

    bash-ble: fallback to bash if not attached

---
+++ example/cmd/_test/bash-ble.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+_example_completion() {
+  export COMP_WORDBREAKS
+
+  local compline="${COMP_LINE:0:${COMP_POINT}}"
+  local IFS=$'\n'
+  mapfile -t COMPREPLY < <(echo "$compline" | sed -e "s/ \$/ ''/" -e 's/"/\"/g' | xargs example _carapace bash)
+  [[ "${COMPREPLY[*]}" == "" ]] && COMPREPLY=() # fix for mapfile creating a non-empty array from empty command output
+
+  [[ ${COMPREPLY[0]} == *[/=@:.,$'\001'] ]] && compopt -o nospace
+  # TODO use mapfile
+  # shellcheck disable=SC2206
+  [[ ${#COMPREPLY[@]} -eq 1 ]] && COMPREPLY=(${COMPREPLY%$'\001'})
+}
+
+
+
+_example_completion_ble() {
+  if [[ ${BLE_ATTACHED-} ]]; then
+    bleopt complete_menu_style=desc
+
+    local compline="${COMP_LINE:0:${COMP_POINT}}"
+    local IFS=$'\n'
+    local c
+    mapfile -t c < <(echo "$compline" | sed -e "s/ \$/ ''/" -e 's/"/\"/g' | xargs example _carapace bash-ble)
+    [[ "${c[*]}" == "" ]] && c=() # fix for mapfile creating a non-empty array from empty command output
+
+    local cand
+    for cand in "${c[@]}"; do
+      [ ! -z "$cand" ] && ble/complete/cand/yield mandb "${cand%$'\t'*}" "${cand##*$'\t'}"
+    done
+  else
+    complete -F _example_completion example
+  fi
+}
+
+complete -F _example_completion_ble example
+

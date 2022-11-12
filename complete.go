2c0e313290bcb0e4851b3a5ac58a716ee2d4619d
rsteube <rsteube@users.noreply.github.com>
2022-11-13 00:38:14 +0100

    removed unnecessary code from snippet creation

--- complete.go
+++ complete.go
@@ -1,8 +1,6 @@
 package carapace
 
 import (
-	"fmt"
-	"io"
 	"strings"
 
 	"github.com/rsteube/carapace/internal/common"
@@ -14,70 +12,60 @@ import (
 )
 
 func complete(cmd *cobra.Command, args []string) (string, error) {
-	if len(args) == 0 {
-		if s, err := Gen(cmd).Snippet(ps.DetermineShell()); err != nil {
-			fmt.Fprintln(io.MultiWriter(cmd.OutOrStderr(), logger.Writer()), err.Error())
-		} else {
-			fmt.Fprintln(io.MultiWriter(cmd.OutOrStdout(), logger.Writer()), s)
+	switch len(args) {
+	case 0:
+		return Gen(cmd).Snippet(ps.DetermineShell())
+	case 1:
+		return Gen(cmd).Snippet(args[0])
+	}
+
+	shell := args[0]
+	current := args[len(args)-1]
+	previous := args[len(args)-2]
+
+	if err := config.Load(); err != nil {
+		return ActionMessage("failed to load config: "+err.Error()).Invoke(Context{CallbackValue: current}).value(shell, current), nil
+	}
+
+	targetCmd, targetArgs, err := findTarget(cmd, args)
+	if err != nil {
+		return ActionMessage(err.Error()).Invoke(Context{CallbackValue: current}).value(shell, current), nil
+	}
+
+	context := newContext(append(targetArgs, current))
+
+	// TODO needs more cleanup and tests
+	var targetAction Action
+	if flag := lookupFlag(targetCmd, previous); !targetCmd.DisableFlagParsing && flag != nil && flag.NoOptDefVal == "" && !common.IsDash(targetCmd) { // previous arg is a flag and needs a value
+		targetAction = storage.getFlag(targetCmd, flag.Name)
+	} else if !targetCmd.DisableFlagParsing && strings.HasPrefix(current, "-") && !common.IsDash(targetCmd) { // assume flag
+		if strings.Contains(current, "=") { // complete value for optarg flag
+			if flag := lookupFlag(targetCmd, current); flag != nil && flag.NoOptDefVal != "" {
+				a := storage.getFlag(targetCmd, flag.Name)
+				splitted := strings.SplitN(current, "=", 2)
+				context.CallbackValue = splitted[1]
+				current = strings.Replace(current, "=", opts.OptArgDelimiter, 1)                  // revert (potentially) overridden optarg divider for `.value()` invocation below
+				targetAction = a.Invoke(context).Prefix(splitted[0] + opts.OptArgDelimiter).ToA() // prefix with (potentially) overridden optarg delimiter
+			}
+		} else { // complete flagnames
+			targetAction = actionFlags(targetCmd)
 		}
 	} else {
-		if len(args) == 1 {
-			s, err := Gen(cmd).Snippet(args[0])
-			if err != nil {
-				return "", err
-			}
-			return s, nil
+		if len(context.Args) > 0 {
+			context.Args = context.Args[:len(context.Args)-1] // current word being completed is a positional so remove it from context.Args
+		}
+
+		if common.IsDash(targetCmd) {
+			targetAction = findAction(targetCmd, targetArgs[targetCmd.ArgsLenAtDash():])
 		} else {
-			shell := args[0]
-			current := args[len(args)-1]
-			previous := args[len(args)-2]
-
-			if err := config.Load(); err != nil {
-				return ActionMessage("failed to load config: "+err.Error()).Invoke(Context{CallbackValue: current}).value(shell, current), nil
+			targetAction = findAction(targetCmd, targetArgs)
+			if targetCmd.HasAvailableSubCommands() && len(targetArgs) <= 1 {
+				subcommandA := actionSubcommands(targetCmd).Invoke(context)
+				targetAction = targetAction.Invoke(context).Merge(subcommandA).ToA()
 			}
-
-			targetCmd, targetArgs, err := findTarget(cmd, args)
-			if err != nil {
-				return ActionMessage(err.Error()).Invoke(Context{CallbackValue: current}).value(shell, current), nil
-			}
-
-			context := newContext(append(targetArgs, current))
-
-			// TODO needs more cleanup and tests
-			var targetAction Action
-			if flag := lookupFlag(targetCmd, previous); !targetCmd.DisableFlagParsing && flag != nil && flag.NoOptDefVal == "" && !common.IsDash(targetCmd) { // previous arg is a flag and needs a value
-				targetAction = storage.getFlag(targetCmd, flag.Name)
-			} else if !targetCmd.DisableFlagParsing && strings.HasPrefix(current, "-") && !common.IsDash(targetCmd) { // assume flag
-				if strings.Contains(current, "=") { // complete value for optarg flag
-					if flag := lookupFlag(targetCmd, current); flag != nil && flag.NoOptDefVal != "" {
-						a := storage.getFlag(targetCmd, flag.Name)
-						splitted := strings.SplitN(current, "=", 2)
-						context.CallbackValue = splitted[1]
-						current = strings.Replace(current, "=", opts.OptArgDelimiter, 1)                  // revert (potentially) overridden optarg divider for `.value()` invocation below
-						targetAction = a.Invoke(context).Prefix(splitted[0] + opts.OptArgDelimiter).ToA() // prefix with (potentially) overridden optarg delimiter
-					}
-				} else { // complete flagnames
-					targetAction = actionFlags(targetCmd)
-				}
-			} else {
-				if len(context.Args) > 0 {
-					context.Args = context.Args[:len(context.Args)-1] // current word being completed is a positional so remove it from context.Args
-				}
-
-				if common.IsDash(targetCmd) {
-					targetAction = findAction(targetCmd, targetArgs[targetCmd.ArgsLenAtDash():])
-				} else {
-					targetAction = findAction(targetCmd, targetArgs)
-					if targetCmd.HasAvailableSubCommands() && len(targetArgs) <= 1 {
-						subcommandA := actionSubcommands(targetCmd).Invoke(context)
-						targetAction = targetAction.Invoke(context).Merge(subcommandA).ToA()
-					}
-				}
-			}
-			return targetAction.Invoke(context).value(shell, current), nil
 		}
 	}
-	return "", nil // TODO
+	return targetAction.Invoke(context).value(shell, current), nil
 }
 
 func findAction(targetCmd *cobra.Command, targetArgs []string) Action {

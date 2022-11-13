b4f5902648be2684fbf89dd11efa6ec082f6344d
rsteube <rsteube@users.noreply.github.com>
2022-11-13 19:03:37 +0100

    tmp

--- internal/traverse/traverse.go
+++ internal/traverse/traverse.go
@@ -3,7 +3,7 @@ package traverse
 import "github.com/spf13/cobra"
 
 func Traverse(cmd *cobra.Command, args []string) (*cobra.Command, error) {
-	return traverse(cmd, args[:len(args)-1])
+	return traverse(cmd, args[:len(args)-1]) // skip currently completed word
 }
 
 func traverse(cmd *cobra.Command, args []string) (*cobra.Command, error) {
@@ -13,6 +13,16 @@ func traverse(cmd *cobra.Command, args []string) (*cobra.Command, error) {
 		return nil, err
 	} else {
 		args = cmd.Flags().Args()
+	}
+
+	for index, arg := range args {
+		if subcommand, _, err := cmd.Find([]string{arg}); err != nil {
+			subArgs := []string{}
+			if index+1 < len(arg) {
+				subArgs = args[index+1:]
+			}
+			traverse(subcommand, subArgs)
+		}
 	}
 
 	return cmd, nil

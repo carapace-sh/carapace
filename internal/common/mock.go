d152f91d2d665a05625d1bd462588052e4a53a0b
rsteube <rsteube@users.noreply.github.com>
2023-07-21 16:15:03 +0200

    sandbox: quickfix for work directory

--- internal/common/mock.go
+++ internal/common/mock.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"fmt"
+	"os"
+)
+
 type Mock struct {
 	Dir     string
 	Replies map[string]string
@@ -11,4 +16,28 @@ func (m Mock) CacheDir() string {
 
 func (m Mock) WorkDir() string {
 	return m.Dir + "/work/"
+}
+
+type t interface {
+	Name() string
+	Fatal(...interface{})
+}
+
+func NewMock(t t) *Mock {
+	tempDir, err := os.MkdirTemp(os.TempDir(), fmt.Sprintf("carapace-sandbox_%v_", t.Name()))
+	if err != nil {
+		t.Fatal("failed to create sandbox dir: " + err.Error())
+	}
+
+	m := &Mock{
+		Dir:     tempDir,
+		Replies: make(map[string]string),
+	}
+	if err := os.Mkdir(m.CacheDir(), os.ModePerm); err != nil {
+		t.Fatal("failed to create sandbox cache dir: " + err.Error())
+	}
+	if err := os.Mkdir(m.WorkDir(), os.ModePerm); err != nil {
+		t.Fatal("failed to create sandbox work dir: " + err.Error())
+	}
+	return m
 }

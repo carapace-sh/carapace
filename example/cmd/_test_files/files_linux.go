9063bed2fc8d22c1d7cf41d4477b1910984b1f6f
rsteube <rsteube@users.noreply.github.com>
2023-08-01 12:12:01 +0200

    added more test files

--- example/cmd/_test_files/files_linux.go
+++ example/cmd/_test_files/files_linux.go
@@ -9,16 +9,19 @@ package testfiles
 //go:generate touch -- backtick_`.txt
 //go:generate touch -- both_quotes_'".txt
 //go:generate touch -- colon_:.txt
+//go:generate touch -- curly-bracket_{.txt
 //go:generate touch -- curly-brackets_{}.txt
 //go:generate touch -- dollar_$.txt
 //go:generate touch -- double-quote_".txt
 //go:generate touch -- hash_#.txt
 //go:generate touch -- pipe_|.txt
 //go:generate touch -- question_?.txt
+//go:generate touch -- round-bracket_(.txt
 //go:generate touch -- round-brackets_().txt
 //go:generate touch -- semicolon_;.txt
 //go:generate touch -- single-quote_'.txt
 //go:generate touch -- "space_ .txt"
+//go:generate touch -- square-bracket_[.txt
 //go:generate touch -- square-brackets_[].txt
 //go:generate touch -- "star_*.txt"
 //go:generate touch -- ~_tilde_prefix.txt

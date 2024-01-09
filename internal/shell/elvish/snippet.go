fb4f80294643e4a8397dc32fe0af53c0e388b011
rsteube <rsteube@users.noreply.github.com>
2024-01-09 01:15:57 +0100

    support both for now

--- internal/shell/elvish/snippet.go
+++ internal/shell/elvish/snippet.go
@@ -19,10 +19,18 @@ func Snippet(cmd *cobra.Command) string {
 			edit:notify (styled "usage: " $completion[DescriptionStyle])$completion[Usage]
 		}
 		put $completion[Candidates] | all (one) | peach {|c|
-			if (eq $c[Description] "") {
-		    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style]) &code-suffix=$c[CodeSuffix] &tag=$c[Tag]
-			} else {
-		    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style])(styled " " $completion[DescriptionStyle]" bg-default")(styled "("$c[Description]")" $completion[DescriptionStyle]) &code-suffix=$c[CodeSuffix] &tag=$c[Tag]
+			try {			
+				if (eq $c[Description] "") {
+		    		edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style]) &code-suffix=$c[CodeSuffix] &tag=$c[Tag]
+				} else {
+		    		edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style])(styled " " $completion[DescriptionStyle]" bg-default")(styled "("$c[Description]")" $completion[DescriptionStyle]) &code-suffix=$c[CodeSuffix] &tag=$c[Tag]
+				}
+			} catch {
+				if (eq $c[Description] "") {
+			    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style]) &code-suffix=$c[CodeSuffix]
+				} else {
+			    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style])(styled " " $completion[DescriptionStyle]" bg-default")(styled "("$c[Description]")" $completion[DescriptionStyle]) &code-suffix=$c[CodeSuffix]
+				}			
 			}
 		}
     }

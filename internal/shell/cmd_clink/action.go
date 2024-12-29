43044d87296d694b2839e01772004f671968f046
rsteube <rsteube@users.noreply.github.com>
2024-12-29 20:16:15 +0100

    tmp

--- internal/shell/cmd_clink/action.go
+++ internal/shell/cmd_clink/action.go
@@ -2,8 +2,9 @@ package cmd_clink
 
 import (
 	"github.com/carapace-sh/carapace/internal/common"
+	"github.com/carapace-sh/carapace/internal/shell/fish"
 )
 
 func ActionRawValues(currentWord string, meta common.Meta, values common.RawValues) string {
-	return "TODO"
+	return fish.ActionRawValues(currentWord, meta, values) // TODO
 }

fab8b0538fedbe851c9797a8619182880e9d44b1
rsteube <rsteube@users.noreply.github.com>
2024-12-29 20:12:16 +0100

    tmp

--- example/cmd/_test/cmd-clink.lua
+++ example/cmd/_test/cmd-clink.lua
@@ -1,7 +1,7 @@
 local function example_completion(word, word_index, line_state, match_builder)
   local compline = string.sub(line_state:getline(), 1, line_state:getcursor())
 
-  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%q", compline) .. " example _carapace cmd-clink ''"):read("*a")
+  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%q", compline) .. " example _carapace cmd-clink '' ''"):read("*a")
   for line in string.gmatch(output, '[^\r\n]+') do
     match_builder:addmatch(string.gsub(line, '\t.*', ""))
   end
--- internal/shell/cmd_clink/snippet.go
+++ internal/shell/cmd_clink/snippet.go
@@ -11,7 +11,7 @@ func Snippet(cmd *cobra.Command) string {
 	result := fmt.Sprintf(`local function %v_completion(word, word_index, line_state, match_builder)
   local compline = string.sub(line_state:getline(), 1, line_state:getcursor())
 
-  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%%q", compline) .. " %v _carapace cmd-clink ''"):read("*a")
+  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%%q", compline) .. " %v _carapace cmd-clink '' ''"):read("*a")
   for line in string.gmatch(output, '[^\r\n]+') do
     match_builder:addmatch(string.gsub(line, '\t.*', ""))
   end

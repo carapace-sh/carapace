3d56d5ac540993b1be9e8b4fd28289b3ae7ebb04
rsteube <rsteube@users.noreply.github.com>
2024-12-29 18:52:06 +0100

    tmp

--- example/cmd/_test/cmd-clink.lua
+++ example/cmd/_test/cmd-clink.lua
@@ -1,20 +1,10 @@
 local function example_completion(word, word_index, line_state, match_builder)
-  args = { "example", "_carapace", "fish", "\"\"" }
-  for i = 2,word_index-1,1 do
-    table.insert(args, string.format("%q" ,line_state:getword(i)))
-  end
+  local compline = string.sub(line_state:getline(), 1, line_state:getcursor())
 
-  -- table.insert(args, string.format("%q", word))  
-  local exploded = string.explode(line_state:getline() .. "a") 
-  word = string.gsub(exploded[#exploded], 'a$', "") 
-  table.insert(args, string.format("%q", word)) 
-
-  output = io.popen(table.concat(args, " ")):read("*a")
+  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%q", compline) .. " example _carapace cmd-clink ''"):read("*a")
   for line in string.gmatch(output, '[^\r\n]+') do
-    -- match_builder:addmatch(line)
     match_builder:addmatch(string.gsub(line, '\t.*', ""))
   end
-
   return true
 end
 

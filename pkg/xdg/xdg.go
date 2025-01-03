8e46726f4c8def1672f2b72708caf5c9fe52e069
rsteube <rsteube@users.noreply.github.com>
2025-01-03 11:49:25 +0100

    xdg: ignore relative paths
    needs: https://github.com/golang/go/issues/68470

--- pkg/xdg/xdg.go
+++ pkg/xdg/xdg.go
@@ -10,7 +10,7 @@ import (
 
 // UserCacheDir returns the user cache base directory.
 func UserCacheDir() (dir string, err error) {
-	if dir = os.Getenv("XDG_CACHE_HOME"); dir == "" {
+	if dir = os.Getenv("XDG_CACHE_HOME"); dir == "" || !filepath.IsAbs(dir) {
 		dir, err = os.UserCacheDir()
 	}
 	dir = filepath.ToSlash(dir)
@@ -19,7 +19,7 @@ func UserCacheDir() (dir string, err error) {
 
 // UserConfigDir returns the user config base directory.
 func UserConfigDir() (dir string, err error) {
-	if dir = os.Getenv("XDG_CONFIG_HOME"); dir == "" {
+	if dir = os.Getenv("XDG_CONFIG_HOME"); dir == "" || !filepath.IsAbs(dir) {
 		dir, err = os.UserConfigDir()
 	}
 	dir = filepath.ToSlash(dir)
@@ -44,7 +44,13 @@ func ConfigDirs() (dirs []string, err error) {
 	}
 
 	if v, ok := os.LookupEnv("XDG_CONFIG_DIRS"); ok {
-		dirs = append(strings.Split(v, string(os.PathSeparator)), dirs...)
+		xdgDirs := make([]string, 0)
+		for _, dir := range strings.Split(v, string(os.PathSeparator)) {
+			if filepath.IsAbs(dir) {
+				xdgDirs = append(xdgDirs, dir)
+			}
+		}
+		dirs = append(xdgDirs, dirs...)
 	}
 
 	for index, dir := range dirs {

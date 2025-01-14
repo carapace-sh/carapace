b28eab833da267d0f2d89223a06ad11e7e8fb3d4
rsteube <rsteube@users.noreply.github.com>
2025-01-14 16:42:03 +0100

    updated compose

--- .dockerfile/home/.config/elvish/rc.elv
+++
@@ -1,7 +0,0 @@
-set-env SHELL elvish
-set-env STARSHIP_SHELL elvish
-set-env LS_COLORS (vivid generate dracula)
-set edit:prompt = { starship prompt }
-if (has-env TARGET) {
-  eval ($E:TARGET _carapace|slurp)
-}
--- .dockerfile/home/.config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-set SHELL 'fish'
-set STARSHIP_SHELL 'fish'
-set LS_COLORS (vivid generate dracula)
-starship init fish | source 
-mkdir -p ~/.config/fish/completions
-set -q TARGET &&  $TARGET _carapace fish | source

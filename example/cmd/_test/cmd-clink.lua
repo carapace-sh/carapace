7273d3605128a8a2ff1e4d9cb90cdee71ecfbf42
rsteube <rsteube@users.noreply.github.com>
2025-09-25 09:19:01 +0200

    clink: update snipped

--- example/cmd/_test/cmd-clink.lua
+++ example/cmd/_test/cmd-clink.lua
@@ -1,23 +1,36 @@
 local function example_completion(word, word_index, line_state, match_builder)
-  local compline = string.sub(line_state:getline(), 1, line_state:getcursor())
+    match_builder:setnosort()
+    match_builder:setvolatile()
+    os.setenv('CARAPACE_COMPLINE', line_state:getline():sub(1, line_state:getcursor()))
 
-  local output = io.popen("env CARAPACE_COMPLINE=" .. string.format("%q", compline) .. " example _carapace cmd-clink \"\""):read("*a")
-  for line in string.gmatch(output, '[^\r\n]+') do
-    local matches = {}
-    for m in string.gmatch(line, '[^\t]+') do
-      table.insert(matches, m)
+    local file, pclose = io.popenyield('example _carapace cmd-clink example')
+
+    if not file then
+        return false
     end
-    match_builder:addmatch({
-      match = matches[1],
-      display = matches[2],
-      description = matches[3],
-      type = "word",
-      appendchar = matches[4],
-      suppressappend = false
-    })
-  end
-  return true
+
+    for line in file:lines() do
+        local matches = string.explode(line, '\t')
+
+        if matches[1] then
+            match_builder:addmatch({
+                match       = matches[1],
+                display     = matches[2],
+                description = matches[3],
+                type        = 'word',
+                appendchar  = matches[4] or ''
+            })
+        end
+    end
+
+    if pclose then
+        pclose()
+    else
+        file:close()
+    end
+
+    return not match_builder:isempty()
 end
 
-clink.argmatcher("example"):addarg({nowordbreakchars="'&backprime;+;,", example_completion}):loop(1)
+clink.argmatcher(50, 'example', 'example.exe'):addarg({nowordbreakchars="'`=+;,", example_completion}):loop(1)
 

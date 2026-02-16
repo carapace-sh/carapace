92ad34a33f47b2628ee68078f7ed4fa5cbeaf7d6
rsteube <rsteube@users.noreply.github.com>
2026-02-16 20:22:37 +0100

    nushell: adopt alias expansion from carapace-bin

--- example/cmd/_test/nushell.nu
+++ example/cmd/_test/nushell.nu
@@ -1,3 +1,14 @@
-let example_completer = {|spans| 
+let example_completer = {|spans|
+    # if the current command is an alias, get it's expansion
+    let expanded_alias = (scope aliases | where name == $spans.0 | $in.0?.expansion?)
+
+    # overwrite
+    let spans = (if $expanded_alias != null  {
+      # put the first word of the expanded alias first in the span
+      $spans | skip 1 | prepend ($expanded_alias | split row " " | take 1)
+    } else {
+      $spans | skip 1 | prepend ($spans.0)
+    })
+
     example _carapace nushell ...$spans | from json
 }
--- internal/shell/nushell/snippet.go
+++ internal/shell/nushell/snippet.go
@@ -10,7 +10,18 @@ import (
 
 // Snippet creates the nushell completion script.
 func Snippet(cmd *cobra.Command) string {
-	return fmt.Sprintf(`let %v_completer = {|spans| 
+	return fmt.Sprintf(`let %v_completer = {|spans|
+    # if the current command is an alias, get it's expansion
+    let expanded_alias = (scope aliases | where name == $spans.0 | $in.0?.expansion?)
+
+    # overwrite
+    let spans = (if $expanded_alias != null  {
+      # put the first word of the expanded alias first in the span
+      $spans | skip 1 | prepend ($expanded_alias | split row " " | take 1)
+    } else {
+      $spans | skip 1 | prepend ($spans.0)
+    })
+
     %v _carapace nushell ...$spans | from json
 }`, cmd.Name(), uid.Executable())
 }

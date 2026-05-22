ab0dfd81b8e26b68a834d387a0d82e66b0c63504
brain-nrds <brain-nrds@users.noreply.github.com>
2026-05-22 16:47:33 -0700

    nushell: upsert external completer in init snippet

--- example/cmd/_test/nushell.nu
+++ example/cmd/_test/nushell.nu
@@ -1,3 +1,15 @@
-let example_completer = {|spans| 
+let example_completer = {|spans|
     example _carapace nushell ...$spans | from json
 }
+
+let example_previous_completer = ($env.config.completions.external.completer? | default {|spans| null })
+$env.config = ($env.config | upsert completions.external {
+    enable: true
+    completer: {|spans|
+        if $spans.0 == "example" {
+            do $example_completer $spans
+        } else {
+            do $example_previous_completer $spans
+        }
+    }
+})
--- internal/shell/nushell/snippet.go
+++ internal/shell/nushell/snippet.go
@@ -10,7 +10,19 @@ import (
 
 // Snippet creates the nushell completion script.
 func Snippet(cmd *cobra.Command) string {
-	return fmt.Sprintf(`let %v_completer = {|spans| 
-    %v _carapace nushell ...$spans | from json
-}`, cmd.Name(), uid.Executable())
+	return fmt.Sprintf(`let %[1]v_completer = {|spans|
+    %[2]v _carapace nushell ...$spans | from json
+}
+
+let %[1]v_previous_completer = ($env.config.completions.external.completer? | default {|spans| null })
+$env.config = ($env.config | upsert completions.external {
+    enable: true
+    completer: {|spans|
+        if $spans.0 == %[1]q {
+            do $%[1]v_completer $spans
+        } else {
+            do $%[1]v_previous_completer $spans
+        }
+    }
+})`, cmd.Name(), uid.Executable())
 }

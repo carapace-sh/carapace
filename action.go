64476646fcc78c65475b42398c8fa802f322e90d
rsteube <rsteube@users.noreply.github.com>
2026-06-30 20:45:49 +0200

    Migrate call sites to SplitForCompletion
    Replace manual token chain patterns with SplitForCompletion:
    
    - zsh/action.go: replace 4-regex quoting detection with ctx.QuotingState
      from SplitForCompletion using ZshFormat. Full-quoting state detection
      (FULL_QUOTING_*_STATE) now uses HasPrefix/HasSuffix on RawCurrentWord
      instead of regexes. Remove regexp import.
    
    - bash/patch.go: replace tokens.CurrentPipeline().FilterRedirects().
      Words().Strings() chain with ctx.Words, redirect detection with
      ctx.IsRedirect, WordbreakPrefix() with ctx.Prefix.
    
    - cmd_clink/patch.go: same chain replacement with ctx.Words.
    
    - action.go:split(): use ctx.Words for pipeline args, ctx.QuotingState
      for value quoting, ctx.IsRedirect/ctx.CurrentWord for redirect target.
      Keep Split + pipelineTokens for prefix computation (Span-based prefix
      differs from ctx.Prefix which is wordbreak prefix).
    
    All tests pass.
    
    Assisted-by: Crush:glm-5.2

--- action.go
+++ action.go
@@ -299,33 +299,33 @@ func (a Action) split(pipelines bool) Action {
 			return ActionMessage(err.Error())
 		}
 
+		ctx := shlex.SplitForCompletion(c.Value, shlex.BashFormat())
+
 		var context Context
 		if pipelines {
-			tokens = tokens.CurrentPipeline()
-			context = NewContext(tokens.FilterRedirects().Words().Strings()...)
+			context = NewContext(ctx.Words...)
 		} else {
 			context = NewContext(tokens.Words().Strings()...)
 		}
 
 		originalValue := c.Value
-		prefix := originalValue[:tokens.Words().CurrentToken().Span.Start]
+		pipelineTokens := tokens.CurrentPipeline()
+		prefix := originalValue[:pipelineTokens.Words().CurrentToken().Span.Start]
 		c.Args = context.Args
 		c.Parts = []string{}
 		c.Value = context.Value
 
-		if pipelines { // support redirects
-			if len(tokens) > 1 && tokens[len(tokens)-2].WordbreakType.IsRedirect() {
-				LOG.Printf("completing files for redirect arg %#v", tokens.Words().CurrentToken().Value)
-				prefix = originalValue[:tokens.CurrentToken().Span.Start]
-				c.Value = tokens.CurrentToken().Value
-				a = ActionFiles()
-			}
+		if pipelines && ctx.IsRedirect {
+			LOG.Printf("completing files for redirect arg %#v", ctx.CurrentWord)
+			prefix = originalValue[:pipelineTokens.CurrentToken().Span.Start]
+			c.Value = ctx.CurrentWord
+			a = ActionFiles()
 		}
 
 		invoked := a.Invoke(c)
 		for index, value := range invoked.action.rawValues {
 			if !invoked.action.meta.Nospace.Matches(value.Value) || strings.Contains(value.Value, " ") { // TODO special characters
-				switch tokens.CurrentToken().State {
+				switch ctx.QuotingState {
 				case shlex.QUOTING_ESCAPING_STATE:
 					invoked.action.rawValues[index].Value = fmt.Sprintf(`"%v"`, strings.ReplaceAll(value.Value, `"`, `\"`))
 				case shlex.QUOTING_STATE:
--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
 module github.com/carapace-sh/carapace
 
-go 1.24
+go 1.24.0
 
 require (
-	github.com/carapace-sh/carapace-shlex v1.1.1
+	github.com/carapace-sh/carapace-shlex v1.1.2-0.20260701213017-3985f5788c03
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/pflag v1.0.10
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require github.com/inconshreveable/mousetrap v1.1.0 // indirect
-
-replace github.com/carapace-sh/carapace-shlex => ../carapace-shlex
